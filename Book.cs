88a6399163dd39309ee20e56dc7f741540be9060
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-20 15:16:54 +0200

    create-dvd-book-classes

---
+++ Book.cs
@@ -0,0 +1,13 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+public class Book : Product
+{
+    private string isbn;
+    private int pages;
+    public Book(string isbn, int pages, string title, int year, string category, bool isBorrowed, int shelfCode, string author) : base(title, year, category, isBorrowed, shelfCode, author)
+    {
+        this.isbn = isbn;
+        this.pages = pages;
+    }
+}
---
+++ Dvd.cs
@@ -0,0 +1,13 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+public class Dvd : Product
+{
+    private int serialNumber;
+    private int duration;
+    public Dvd(int serialNumber, int duration, string title, int year, string category, bool isBorrowed, int shelfCode, string author) : base(title, year, category, isBorrowed, shelfCode, author)
+    {
+        this.serialNumber = serialNumber;
+        this.duration = duration;
+    }
+}
